643903bb5efebf306b83151e59ce3993f07032c0
GodNate <kornbepstaffs@gmail.com>
2022-09-22 11:47:18 +0800

    music's

---
+++ bot/index.js
@@ -0,0 +1,102 @@
+const { Client } = require('discord.js');
+const { Manager } = require('erela.js');
+const fs = require('fs');
+
+const config = require('./config.json');
+const commands = require('./handlers/commands');
+const logger = require('./util/logger');
+
+const djsEvents = require('./events/discord');
+const ejsEvents = require('./events/erela');
+
+const Spotify = require('erela.js-spotify');
+
+
+const ansiEscRegEx = /\u001b\[\d+m/g;
+
+if (config.production === true) {
+    const fstream = fs.createWriteStream('musicbot.log');
+    const fsw = fstream.write.bind(fstream);
+
+    // replace write functions and filter out ansi escape codes
+    process.stdout.write = process.stderr.write = (...args) => {
+        fsw(...args.map(a => {
+            if (typeof a !== 'string') return a;
+            return a.replace(ansiEscRegEx, '');
+        }));
+    }
+}
+
+const plugins = [];
+
+if (!config.token || !config.clientId) {
+    logger.error('Missing token or clientId in config.json');
+    process.exit(0);
+}
+if (!config.spotifyId || !config.spotifySecret) {
+    logger.warn('Missing spotifyId or spotifySecret in config.json, disabling Spotify support.');
+}
+else {
+    plugins.push(new Spotify({
+        clientID: config.spotifyId,
+        clientSecret: config.spotifySecret
+    }));
+}
+
+// Initialize the bot
+const bot = new Client({
+    intents: 32767
+});
+
+bot.config = config;
+
+
+// ErelaJS Setup
+bot.manager = new Manager({
+    nodes: [{
+        host: 'lavalink.cloudblue.ml',
+        password: 'danbotbest',
+        port: 1555,
+        retryDelay: 5000
+    }],
+    autoPlay: true,
+    send: (id, payload) => {
+        const guild = bot.guilds.cache.get(id);
+        if (guild) guild.shard.send(payload);
+    },
+    plugins: plugins
+});
+
+
+const activities = [
+    ['LISTENING', '/play'],
+    ['PLAYING', 'sick beats'],
+    ['PLAYING', 'Music'],
+]
+
+bot.on('ready', () => {
+
+    logger.info('Bot is ready!');
+
+    // Initialize Erela Manager
+    bot.manager.init(bot.user.id);
+
+    // Command Handler
+    commands.setup();
+
+    // Events
+    djsEvents.setup(bot);
+    ejsEvents.setup(bot);
+
+    setInterval(() => {
+        const activity = activities[Math.floor(Math.random() * activities.length)];
+        bot.user.setActivity(activity[1], { type: activity[0] });
+    }, 15_000);
+});
+
+module.exports = {
+    bot
+}
+
+// Connect to Discord
+if (require.main === module) bot.login(config.token);
